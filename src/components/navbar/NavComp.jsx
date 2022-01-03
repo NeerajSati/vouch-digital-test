a15909fdce0959b2d42205bc9b2d933e7c8c25e9
Neeraj Sati <neeraj.sati123@gmail.com>
2022-01-03 17:16:55 +0530

    Navbar Completed

--- src/components/navbar/NavComp.jsx
+++ src/components/navbar/NavComp.jsx
@@ -1,18 +1,40 @@
-import React from 'react'
-import './navcomp.css'
-import { Navbar, Nav, Button } from 'react-bootstrap';
+import React from "react";
+import "./navcomp.css";
+import { Navbar, Nav, Button } from "react-bootstrap";
 function NavComp() {
-    return (
-        <Navbar className="navComp" bg="light" expand="sm">
-    <Navbar.Brand href="." className="ml-5"><h1 className="brand">ATools<span className='red'>.</span></h1></Navbar.Brand>
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ms-auto">
-      <Button style={{backgroundColor:"#023047", border: "none"}}>Start Free Trial</Button>
-      <Button style={{backgroundColor:"#FB8500", border: "none"}}>Login</Button>
-      </Nav>
-    </Navbar.Collapse>
-</Navbar>
-    )
+  return (
+    <Navbar className="navComp" bg="light" expand="sm">
+      <Navbar.Brand href="." className="ml-5">
+        <h1 className="brand">
+          ATools<span className="red">.</span>
+        </h1>
+      </Navbar.Brand>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          <Button
+            style={{
+              backgroundColor: "#023047",
+              border: "none",
+              marginRight: "20px",
+              width: "180px",
+            }}
+          >
+            Start Free Trial
+          </Button>
+          <Button
+            style={{
+              backgroundColor: "#FB8500",
+              border: "none",
+              marginRight: "20px",
+              width: "180px",
+            }}
+          >
+            Login
+          </Button>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
-export default NavComp
+export default NavComp;
