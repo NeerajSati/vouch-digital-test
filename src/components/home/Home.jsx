10fd1004ea98ca3caaa6db3f4e789a5139d0a2b6
Neeraj Sati <neeraj.sati123@gmail.com>
2022-01-03 21:07:39 +0530

    HomePage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,42 +1,82 @@
 import React from "react";
-import { Container, Row, Col, Image, Stack, Form, Button } from "react-bootstrap";
+import './home.css';
+import {
+  Row,
+  Col,
+  Image,
+  Stack,
+  Form,
+  Button,
+} from "react-bootstrap";
 function Home() {
   return (
-    <Container style={{
-        margin: "0"}}>
-      <Row >
-        <Col xs={6} style={{
-            alignSelf: "center", textAlign: "center"}}>
-        <Stack gap={2} className="col-md-15 mx-auto">
-        <h1 style={{color: "#023047", marginBottom: "0"}}><strong>Welcome Back!</strong></h1>
-        <p style={{marginTop: "0"}}>PLEASE LOGIN TO CONTINUE!</p> 
-        <Form.Control
-            className="me-auto"
-            placeholder="Email Address*"
-            style={{
-                width: "300px", margin:"auto"}}
-          />
-          <Form.Control
-            className="me-auto"
-            placeholder="Password*"
-            style={{
-                width: "300px", margin:"auto"}}
-                type="password"
-          />
-          <Button style={{
-              backgroundColor: "#023047",
-              border: "none", width: "300px", margin:"auto", marginTop:"10px"
-            }}>Login</Button>
-        </Stack>
+    <div class="home"
+      style={{
+        margin: "0",
+        overflow: "hidden",
+        width: "100%",
+        maxWidth: "auto"
+      }}
+    >
+      <Row className="rowclass">
+        <Col
+          style={{
+            alignSelf: "center",
+            textAlign: "center"
+          }}
+        >
+          <Stack gap={2} className="col-md-15 mx-auto hidestk">
+            <h1 style={{ color: "#023047", marginBottom: "0" }}>
+              <strong>Welcome Back!</strong>
+            </h1>
+            <p style={{ marginTop: "0" }}>PLEASE LOGIN TO CONTINUE!</p>
+            <Form.Control
+              className="me-auto"
+              placeholder="Email Address*"
+              style={{
+                width: "300px",
+                margin: "auto",
+              }}
+            />
+            <Form.Control
+              className="me-auto"
+              placeholder="Password*"
+              style={{
+                width: "300px",
+                margin: "auto",
+              }}
+              type="password"
+            />
+            <Button
+              style={{
+                backgroundColor: "#023047",
+                border: "none",
+                width: "300px",
+                margin: "auto",
+                marginTop: "10px",
+              }}
+            >
+              Login
+            </Button>
+            <div className="passcheck">
+            <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Remember Password" />
+  </Form.Group>
+  <Form.Text className="text-muted">
+      Forgot Password?
+    </Form.Text>
+  </div>
+          </Stack>
         </Col>
-        <Col xs={6}>
+        <Col xs={6} 
+        className="bgimage">
           <Image
             src="https://cdn.pixabay.com/photo/2021/11/19/15/21/christmas-6809681_960_720.png"
             responsive
           />
         </Col>
       </Row>
-    </Container>
+    </div>
   );
 }
 
